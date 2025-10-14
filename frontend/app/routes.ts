98979eeb0dd1fae599bb48b4d721b218b71a38a0
Leigh Ann Walker <leighannbeck06@gmail.com>
2025-10-14 15:14:48 -0600

    set template for routes

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -1,3 +1,8 @@
 import { type RouteConfig, index } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    index("routes/home/home.tsx"),
+    // route('/community', 'routes/community.tsx'),
+    // route('/profile/:username', 'routes/profile.tsx'),
+
+] satisfies RouteConfig;
