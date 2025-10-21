7b3a78a80793fb48a97cd3b8027da5e512fbaed7
Eric Manolito <emanolito@cnm.edu>
2025-10-21 16:10:43 -0600

    new branch to work on community and scrapbook pages
    # Conflicts:
    #	frontend/app/routes/scrapbook/scrapbook.tsx

--- frontend/app/routes/scrapbook/scrapbook.tsx
+++ frontend/app/routes/scrapbook/scrapbook.tsx
@@ -1,4 +1,7 @@
-
+import {Button, Carousel} from "flowbite-react";
+import { Banner, BannerCollapseButton } from "flowbite-react";
+import { HiX } from "react-icons/hi";
+import { MdAnnouncement } from "react-icons/md";
 import { Card } from "flowbite-react";
 
 
@@ -6,96 +9,174 @@ import { Card } from "flowbite-react";
 export default function Scrapbook () {
     return(
         <>
-            <div className="flex w-full justify-between border-b border-gray-200 bg-blue-500 p-4 dark:border-gray-600 dark:bg-gray-700">
+            <div className="flex w-full justify-between border-gray-200 bg-blue-500 p-4">
                 <div className="mx-auto flex items-center">
-                    <p className="flex items-center font-normal dark:text-gray-400 text-3xl text-white whitespace-nowrap">
-                        Space for your collection of memories
-                    </p>
+                    <h1 className="font-bold whitespace-nowrap text-3xl text-white ">
+                        Your Collection of Memories
+                    </h1>
                 </div>
             </div>
-
-            <section className='w-full bg-amber-300 flex flex-cols-3 gap-5 md:' >
-                <h2 className='text-3xl' >Travel</h2>
-
-                <Card className="max-w-sm mt-2"
-                imgAlt="placeholder photo"
-                imgSrc='https://placehold.co/100x100'
-                >
+            <section className='w-full bg-gray-200 px-4 pt-5' >
+                <div className='flex flex-col items-center mb-6'>
+                    <h2 className='text-3xl font-extrabold text-center mb-6' >Travel</h2>
+                    <Button>See Gallery</Button>
+                </div>
+                <div className='flex flex-wrap justify-center gap-6' >
+                    <Card className="max-w-sm mt-2" imgAlt="hot air balloons at sunset" imgSrc='img_4.png'>
                 <h5 className="text-xl font-bold whitespace-nowrap tracking-tight text-gray-900 dark:text-white">
-                Albuquerque Balloon Fiesta 2024
+                    Albuquerque Balloon Fiesta 2024
                 </h5>
-                <p className="font-normal text-gray-300 dark:text-gray-400">
-                Finally completed my bucket item of a Hot Air Balloon experience.
+                 <p className="font-normal text-gray-300 dark:text-gray-400">
+                 Finally completed my bucket item of a Hot Air Balloon experience.
                 </p>
+                    <Button>Submit</Button>
                 </Card>
-
                 <Card className="max-w-sm mt-2"
-                      imgAlt="placeholder photo"
-                      imgSrc='https://placehold.co/100x100'>
+                      imgAlt="person on hiking trail"
+                      imgSrc='img_5.png'>
                     <h5 className="text-xl font-bold whitespace-nowrap tracking-tight text-gray-900 dark:text-white">
                         Sandia Mountains Hiking Trail 2025
                     </h5>
                     <p className="font-normal text-gray-300 dark:text-gray-400">
                         Completed 12,000 steps in a single day hike!
                     </p>
+                    <Button>Submit</Button>
                 </Card>
                 <Card className="max-w-sm mt-2"
-                      imgAlt="placeholder photo"
-                      imgSrc='https://placehold.co/100x100'>
+                      imgAlt="highway sunset view"
+                      imgSrc='img_6.png'>
                     <h5 className="text-xl font-bold whitespace-nowrap tracking-tight text-gray-900 dark:text-white">
                         Route 66 2025
                     </h5>
                     <p className="font-normal text-gray-300 dark:text-gray-400">
                         10 days traveling on route 66
                     </p>
+                    <Button>Submit</Button>
                 </Card>
-                <Card className="max-w-sm mt-2"
-                      imgAlt="placeholder photo"
-                      imgSrc='https://placehold.co/100x100'
-                >
-                    <h5 className="text-xl font-bold whitespace-nowrap tracking-tight text-gray-900 dark:text-white">
-                        Albuquerque Balloon Fiesta 2024
-                    </h5>
-                    <p className="font-normal text-gray-300 dark:text-gray-400">
-                        Finally completed my bucket item of a Hot Air Balloon experience.
-                    </p>
-                </Card>
-
-                <Card className="max-w-sm mt-2"
-                      imgAlt="placeholder photo"
-                      imgSrc='https://placehold.co/100x100'>
-                    <h5 className="text-xl font-bold whitespace-nowrap tracking-tight text-gray-900 dark:text-white">
-                        Sandia Mountains Hiking Trail 2025
-                    </h5>
-                    <p className="font-normal text-gray-300 dark:text-gray-400">
-                        Completed 12,000 steps in a single day hike!
-                    </p>
-                </Card>
-                <Card className="max-w-sm mt-2"
-                      imgAlt="placeholder photo"
-                      imgSrc='https://placehold.co/100x100'>
-                    <h5 className="text-xl font-bold whitespace-nowrap tracking-tight text-gray-900 dark:text-white">
-                        Route 66 2025
-                    </h5>
-                    <p className="font-normal text-gray-300 dark:text-gray-400">
-                        10 days traveling on route 66
-                    </p>
-                </Card>
+                </div>
+            </section>
+                <hr/>
+            <section className="w-full bg-gray-400 py-6">
+                <div className="flex flex-col md:flex-row justify-between gap-6 px-4">
+                    <div className="flex-1">
+                        <div className='flex flex-col items-center mb-6'>
+                        <h2 className="text-3xl mb-4">Health & Fitness</h2>
+                        <Button>See Gallery</Button>
+                        </div>
+                        <div className="grid grid-cols-1 md:grid-cols-2 justify-center gap-4">
+                            <Card className="max-w-sm" imgAlt="highway sunset view"
+                                  imgSrc='img_6.png'>
+                                <h5 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                    Route 66 2025
+                                </h5>
+                                <p className="font-normal text-gray-300 dark:text-gray-400">
+                                    10 days traveling on route 66
+                                </p>
+                                <Button>Default</Button>
+                            </Card>
+                            <Card className="max-w-sm" imgAlt="placeholder photo"
+                                  imgSrc='img_6.png'>
+                                <h5 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                    Route 66 2025
+                                </h5>
+                                <p className="font-normal text-gray-300 dark:text-gray-400">
+                                    10 days traveling on route 66
+                                </p>
+                                <Button>Default</Button>
+                            </Card>
+                        </div>
+                    </div>
+                    <div className="flex-1">
+                        <div className='flex flex-col items-center mb-6'>
+                        <h2 className="text-3xl mb-4">Learning</h2>
+                        <Button>See Gallery</Button>
+                        </div>
+                        <div className="grid grid-cols-1 md:grid-cols-2 justify-center gap-4">
+                            <Card className="max-w-sm" imgAlt="placeholder photo"
+                                  imgSrc='img_6.png'>
+                                <h5 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                    Route 66 2025
+                                </h5>
+                                <p className="font-normal text-gray-300 dark:text-gray-400">
+                                    10 days traveling on route 66
+                                </p>
+                                <Button>Default</Button>
+                            </Card>
+                            <Card className="max-w-sm" imgAlt="placeholder photo"
+                                  imgSrc='img_6.png'>
+                                <h5 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                    Route 66 2025
+                                </h5>
+                                <p className="font-normal text-gray-300 dark:text-gray-400">
+                                    10 days traveling on route 66
+                                </p>
+                                <Button>Default</Button>
+                            </Card>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="w-full bg-white py-6">
+                <div className="flex flex-col md:flex-row justify-between gap-6 px-4">
+                    <div className="flex-1">
+                        <div className='flex flex-col items-center mb-6'>
+                            <h2 className="text-3xl mb-4">Food</h2>
+                            <Button>See Gallery</Button>
+                        </div>
+                        <div className="grid grid-cols-1 md:grid-cols-2 justify-center gap-4">
+                            <Card className="max-w-sm" imgAlt="highway sunset view"
+                                  imgSrc='img_6.png'>
+                                <h5 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                    Route 66 2025
+                                </h5>
+                                <p className="font-normal text-gray-300 dark:text-gray-400">
+                                    10 days traveling on route 66
+                                </p>
+                                <Button>Default</Button>
+                            </Card>
+                            <Card className="max-w-sm" imgAlt="placeholder photo"
+                                  imgSrc='img_6.png'>
+                                <h5 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                    Route 66 2025
+                                </h5>
+                                <p className="font-normal text-gray-300 dark:text-gray-400">
+                                    10 days traveling on route 66
+                                </p>
+                                <Button>Default</Button>
+                            </Card>
+                        </div>
+                    </div>
+                    <div className="flex-1">
+                        <div className='flex flex-col items-center mb-6'>
+                            <h2 className="text-3xl mb-4">Group Goals???</h2>
+                            <Button>See Gallery</Button>
+                        </div>
+                        <div className="grid grid-cols-1 md:grid-cols-2 justify-center gap-4">
+                            <Card className="max-w-sm" imgAlt="placeholder photo"
+                                  imgSrc='img_6.png'>
+                                <h5 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                    Route 66 2025
+                                </h5>
+                                <p className="font-normal text-gray-300 dark:text-gray-400">
+                                    10 days traveling on route 66
+                                </p>
+                                <Button>Default</Button>
+                            </Card>
+                            <Card className="max-w-sm" imgAlt="placeholder photo"
+                                  imgSrc='img_6.png'>
+                                <h5 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                    Route 66 2025
+                                </h5>
+                                <p className="font-normal text-gray-300 dark:text-gray-400">
+                                    10 days traveling on route 66
+                                </p>
+                                <Button>Default</Button>
+                            </Card>
+                        </div>
+                    </div>
+                </div>
             </section>
 
-
-{/*<div className="w-full h-[500px] sm:h-64 xl:h-80 2xl:h-96">*/}
-            {/*<Carousel>*/}
-            {/*<img src="" alt="..." />*/}
-            {/*<img src="https://placehold.co/600x400" alt="placeholder image" />*/}
-            {/*<img src="https://placehold.co/600x400" alt="placeholder image" />*/}
-            {/*<img src="https://placehold.co/600x400" alt="placeholder image" />*/}
-            {/*<img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />*/}
-            {/*</Carousel>*/}
-            {/*</div>*/}
         </>
-
-
     )
-
 }
