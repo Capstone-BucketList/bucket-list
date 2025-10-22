04b0539685e47f3b05d7de139d7525b7de9c5138
Leigh Ann Walker <lwalker32@cnm.edu>
2025-10-22 16:45:14 -0600

    working on bucket list item component

--- frontend/app/components/navigation.tsx
+++ frontend/app/components/navigation.tsx
@@ -15,10 +15,11 @@ import {
 export function Navigation() {
     return (
         <>
+
                    <Navbar fluid>
                    <NavbarBrand href="https://flowbite-react.com">
                    {/*<img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />*/}
-                   <span className="self-center whitespace-nowrap text-4xl font-semibold">Bucket List</span>
+                   <span className="self-center whitespace-nowrap text-4xl font-semibold text-pale-tan">WanderList</span>
                    </NavbarBrand>
                    <div className="flex md:order-2">
                    <Dropdown
