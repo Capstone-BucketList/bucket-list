7876380154254def6e0bec66c615f8874e3d9846
Leigh Ann Walker <leighannbeck06@gmail.com>
2025-10-24 12:43:45 -0600

    fixed broken route

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -5,6 +5,7 @@ export default [
     route('/community', 'routes/community/community.tsx'),
     route('/scrapbook', 'routes/scrapbook/scrapbook.tsx'),
     route('/profile', 'routes/profile/profile.tsx'),
+    route('/dashboard', 'routes/dashboard/dashboard.tsx'),
     route('/signup','routes/signup/signup.tsx'),
     route('/login','routes/login/login.tsx'),
 
