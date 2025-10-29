419f7358b35414c981d4f0f14d2d11f8653263dd
Eric Manolito <emanolito@cnm.edu>
2025-10-29 15:21:01 -0600

    adding

--- backend/src/apis/index.controller.ts
+++ backend/src/apis/index.controller.ts
@@ -1,5 +1,8 @@
 import type { Request, Response } from 'express'
+import FormData from "form-data"; // form-data v4.0.1
+import Mailgun from "mailgun.js"; // mailgun.js v11.1.0
 
-export function indexController (request: Request, response: Response): void {
-	response.json('🤯 😬 😱')
+export async function indexController (request: Request, response: Response) {
+    response.json('🤯 😬 😱')
 }
+
--- backend/src/apis/sign-up/sign-up.controller.ts
+++ backend/src/apis/sign-up/sign-up.controller.ts
@@ -3,7 +3,7 @@ import { zodErrorResponse } from '../../utils/response.utils'
 import type {Request} from "express"
 import type {Response} from "express"
 import {type Status} from "../../utils/interfaces/Status.ts";
-import formData from 'form-data'
+
 import Mailgun from 'mailgun.js'
 import {setHash} from "../../utils/auth.utils.ts";
 import {setActivationToken} from "../../utils/auth.utils.ts";
@@ -11,7 +11,6 @@ import {insertProfile, type PrivateProfile} from "../profile/profile.model.ts";
 
 
 export async function signUpProfileController (request:Request, response: Response) {
-    console.log(process.env.MAILGUN_DOMAIN)
     try {
         //validate the new user's data coming from the request body
         const validationResult = SignUpProfileSchema.safeParse(request.body)
@@ -21,6 +20,7 @@ export async function signUpProfileController (request:Request, response: Respon
             return
         }
 
+        console.log(process.env)
         //if validation succeeds, create a new user
         const {userName, email, password, id} = validationResult.data
         const passwordHash = await setHash(password)
@@ -40,22 +40,24 @@ export async function signUpProfileController (request:Request, response: Respon
         await insertProfile(profile)
 
         //prepare and send activation email to new user, by create new mailgun client with mailgun api key
-        const mailgun: Mailgun = new Mailgun(formData)
-        const mailgunClient = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY as string })
         const basePath: string = `${request.protocol}://${request.hostname}:8080${request.originalUrl}activation/${activationToken}`
+
+        // create a new mailgun client with the mailgun api key
+        const mailgun: Mailgun = new Mailgun(FormData)
+        const mailgunClient = mailgun.client({ username: 'api', key: process.env.MAILGUN_API_KEY as string })
 
         const message = `
                 <h2>Welcome to Wander List.</h2>
                 <p>In order to start creating your own Wander List you must confirm your account </p>
                 <a href="${basePath}">${basePath}</a>`
+
         const mailgunMessage = {
             from: `Mailgun Sandbox <postmaster@${process.env.MAILGUN_DOMAIN as string}>`,
             to: email,
-            subject: `One step closer to Wander List --Account Activation`,
+            subject: 'One step closer to Sticky Head -- Account Activation',
             html: message
         }
-
-        await mailgunClient.messages.create(process.env.MAILGUN_DOMIN as string, mailgunMessage)
+        await mailgunClient.messages.create(process.env.MAILGUN_DOMAIN as string, mailgunMessage)
 
         const status: Status = {
             status: 200,
@@ -65,7 +67,6 @@ export async function signUpProfileController (request:Request, response: Respon
         response.status(200).json(status)
 
     }catch(error: any) {
-        console.error(error)
         const status: Status = {
             status: 500,
             message: error.message,
