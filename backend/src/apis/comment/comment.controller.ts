8072b751180e85f7e20703ee1f81b402fd004950
Leigh Ann Walker <leighannbeck06@gmail.com>
2025-11-05 16:58:17 -0700

    resolved errors for comment controller

--- backend/src/apis/comment/comment.controller.ts
+++ backend/src/apis/comment/comment.controller.ts
@@ -1,44 +1,53 @@
 import express, {response} from "express";
-import {type Comment, CommentSchema, postComment} from "./comment.model.ts";
-import {zodErrorResponse} from "../../utils/response.utils.ts";
+import {z} from 'zod/v4'
+import {type Comment, CommentSchema, insertComment, postComment} from "./comment.model.ts";
+import {serverErrorResponse, zodErrorResponse} from "../../utils/response.utils.ts";
 import type {Status} from "../../utils/interfaces/Status.ts";
 import type {Request, Response} from "express";
 
+/**
+ * express controller for creating a new comment
+ * @endpoint POST /apis/comment
+ * @param request and object containing the body with comment data
+ * @param response an object modeling the response that will be sent to the client
+ * @returns response to the client indicating whether the thread creation was successful or not
+ */
 
-
-export async function postCommentController(request: Request, response: Response) :Promise<void> {
+export async function postCommentController(request: Request, response: Response): Promise<void> {
     try {
-        const validationResult = CommentSchema.safeParse(request.body);
 
+        // validate the comment object from the request body
+        const validationResult = CommentSchema.safeParse(request.body)
+
+        // if the validation is unsuccessful, return a preformatted response to the client
         if (!validationResult.success) {
             zodErrorResponse(response, validationResult.error)
             return
         } // end if statement
 
-        const {id, postId, profileId, comment, dateCreated} = validationResult.data
-
-        const comment: Comment = {
-            id,
-            postId,
-            profileId,
-            comment,
-            dateCreated
+        // get the profile from the session
+        const profile = request.session?.profile
+        if (!profile) {
+            response.json({ status: 401, message: 'Please login to leave a comment', data: null})
+            return
         }
 
-        const message = await postComment(comment)
+        // validate that the profileId in the request matches the session profileId
+        if (validationResult.data.profileId !== profile.id) {
+            response.json({ status: 403, message: 'Profile ID in request does not match authenticated user', data: null})
+        }
 
+        // insert the comment into the database
+        const comment = await insertComment(validationResult.data)
+
+        // return success response
         const status: Status = {
-            status: 200,
-            message: message,
-            data: null
+            status: 200, message, data: null
         }
-        response.status(200).json(status)
+        response.json(status)
+
     } catch (error: any) {
-        const status: Status = {
-            status: 500,
-            message: error.message,
-            data: null
-        }
-        response.status(500).json(status)
+        console.error(error)
+        serverErrorResponse(response, error.message)
     }
 }
--- backend/src/apis/comment/comment.model.ts
+++ backend/src/apis/comment/comment.model.ts
@@ -15,33 +15,35 @@ export const CommentSchema = z.object({
     id: z.uuidv7('Please provide a valid ID'),
     postId: z.uuidv7('Please provide a valid post ID'),
     profileId: z.uuidv7('Please provide a valid profile ID'),
-    comment: z.string('Please provide a valid comment'),
-    dateCreated: z.date('Please provide a valid date')
-}).omit({dateCreated:true})
+    comment: z.string('Please provide a valid comment')
+        .min(1, 'Please provide a valid comment')
+        .max(160, 'Comment cannot exceed 160 characters')
+        .trim(),
+    dateCreated: z.coerce.date('Please provide a valid date')
+})
 
 /**
  * this type is used to represent comment object
- * @shape id: string for primary key of the comment
- * @shape postId: string for foreign key referencing post id
- * @shape profileId: string for foreign key referencing profile id
- * @shape comment: string for the comment on a post
- * @shape dateCreated: date for the comment creation date
  */
 
 export type Comment = z.infer<typeof CommentSchema>
 
 /**
- * insert record to comment on post
- * @param comment object to insert
- * @returns string successfully insert message
+ * insert new comment into database
+ * @param comment the comment object to insert
+ * @returns "Comment successfully created"
  */
 
-export async function postComment(comment: Comment) {
+export async function insertComment(comment: Comment): Promise<string> {
+    // validate the thread object against the CommentSchema
     CommentSchema.parse(comment)
 
     const {id, postId, profileId, comment, dateCreated} = comment
 
-    await sql `INSERT INTO comment (id, post_id, profile_id, comment, date_created) VALUES (${id}, ${postId}, ${profileId}, ${comment}, date(${dateCreated}))`
+    await sql `
+        INSERT INTO comment (id, post_id, profile_id, comment, date_created) 
+        VALUES (${comment.id}, ${comment.postId}, ${comment.profileId}, ${comment.comment}, date(${comment.dateCreated}))
+        `
 
-    return ("Comment successfully posted")
+    return ("Comment successfully created")
 }
